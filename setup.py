d343c23d0e336a06efb658531ee766070cec3135
Arif <60351443+protesq@users.noreply.github.com>
2026-02-27 11:52:42 +0300

    fix: include names.txt and lastnames.txt in package data

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="neym",
-    version="0.1.3",
+    version="0.1.4",
     description="A simple name generator library",
     author="Developed by Arif",
     license="MIT",
     python_requires=">=3.8",
     packages=find_packages(),
+    package_data={"neym.core": ["*.txt"]},
+    include_package_data=True,
 )
