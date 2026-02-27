7e4b108d18ae1b0f4e10608dbcc6c44ddbd33923
Arif <60351443+protesq@users.noreply.github.com>
2026-02-27 13:10:20 +0300

    fix: add long_description from README for PyPI project page

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="neym",
-    version="0.1.4",
-    description="A simple name generator library",
+    version="0.1.5",
+    description="A simple Turkish name generator library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Developed by Arif",
     license="MIT",
     python_requires=">=3.8",
